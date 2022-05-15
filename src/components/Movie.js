6c084564adc91b6fa2cfb71b237d83b3ad77292f
seog-jun <shong69@myseneca.ca>
2022-05-14 22:16:06 -0400

    Updated

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,35 @@
 /*eslint-disable*/
-import { Col  } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import noImage from './noImage.png'
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import noImage from "./noImage.png";
 
-export default function Movie(props){
-
-    function poster(){
-        if(props.movie[props.i].poster)
-        {
-            return props.movie[props.i].poster;
-         }
-        else
-        {   
-            return noImage;
-        }
+export default function Movie(props) {
+  function poster() {
+    if (props.movie[props.i].poster) {
+      return props.movie[props.i].poster;
+    } else {
+      return noImage;
     }
-    
+  }
 
-    return(
-        <>       
-         {console.log("movie")}
-        <Col>
-            <Link className="linkStyle" to={'/movies/'+props.movie[props.i]._id} >
-            <img width={150} height = {220} src={poster()} onError={(e)=>{e.target.onerror = null; e.target.src=noImage}}/>
-            <p className="movieName">{props.movie[props.i].title}</p>
-            <p>{props.movie[props.i].year}</p>
-            </Link>
-        </Col>
-        </>
-
-    )
+  return (
+    <>
+      {console.log("movie")}
+      <Col>
+        <Link className="linkStyle" to={"/movies/" + props.movie[props.i]._id}>
+          <img
+            width={150}
+            height={220}
+            src={poster()}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = noImage;
+            }}
+          />
+          <p className="movieName">{props.movie[props.i].title}</p>
+          <p>{props.movie[props.i].year}</p>
+        </Link>
+      </Col>
+    </>
+  );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,137 +1,120 @@
 /*eslint-disable*/
-import Movie from './Movie.js';
-import { Container, Row, Dropdown,  Pagination} from 'react-bootstrap';
-import queryString from 'query-string';
+import Movie from "./Movie.js";
+import { Container, Row, Dropdown, Pagination } from "react-bootstrap";
+import queryString from "query-string";
 import axios from "axios";
-import Loading from './Loading';
-import { useEffect, useState} from 'react';
-import { Card} from 'react-bootstrap';
-import MovieNavBar from './MovieNavBar';
+import Loading from "./Loading";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import MovieNavBar from "./MovieNavBar";
 
-export default function Movies(){
+export default function Movies() {
+  const perPage = 28;
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [render, setRender] = useState(false);
 
-    const perPage = 28;
-    const [page, setPage] = useState(1);
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [render,setRender] = useState(false);
-    
-    
   useEffect(() => {
-    
-  
     let url;
     let query = queryString.parse(location.search);
-    
+
     if (query.title) {
-        url = `https://mflix-jun.herokuapp.com/api/movies?title=${query.title}`;
-      }
-    else {
+      url = `https://mflix-jun.herokuapp.com/api/movies?title=${query.title}`;
+    } else {
       url = `https://mflix-jun.herokuapp.com/api/movies?page=${page}&perPage=${perPage}`;
     }
 
     axios(url)
-    .then((response) => {
-    setData(response.data);
-    
-    })
-    .catch((error) => {
-      console.error("Error fetching data: ", error);
-      setError(error);
-    })
-    .finally(() => {setLoading(false);
-    });
-    
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
     return setRender(false);
-   
-    }, [page,error,render]);
+  }, [page, error, render]);
 
-    
-    if (loading) return (<Loading />);
-    if (error) return  (
-      <Card className='p-3'style={{ width: '100rem' }}>
-          <Card.Title>Something is wrong while fetching data</Card.Title>
-          <Card.Text>{error.message}</Card.Text>
+  if (loading) return <Loading />;
+  if (error)
+    return (
+      <Card className="p-3" style={{ width: "100rem" }}>
+        <Card.Title>Something is wrong while fetching data</Card.Title>
+        <Card.Text>{error.message}</Card.Text>
       </Card>
-      );
-    
+    );
 
-    function descendingOrder(){
-      var movieArray = [...data];
-      var descendingArray = movieArray.sort((a,b)=>{
-        return b.year-a.year;
-      });
-      setData(descendingArray);
-    };
+  function descendingOrder() {
+    var movieArray = [...data];
+    var descendingArray = movieArray.sort((a, b) => {
+      return b.year - a.year;
+    });
+    setData(descendingArray);
+  }
 
+  function ascendingOrder() {
+    var movieArray = [...data];
+    var ascendingArray = movieArray.sort((a, b) => {
+      return a.year - b.year;
+    });
+    setData(ascendingArray);
+  }
 
-    function ascendingOrder(){
-      var movieArray = [...data];
-      var ascendingArray = movieArray.sort((a,b)=>{
-        return a.year-b.year;
-      });
-      setData(ascendingArray);
-    };
+  function previousPage() {
+    if (page > 1) setPage(page - 1);
+  }
 
+  function nextPage() {
+    setPage(page + 1);
+  }
 
-    function previousPage() {
-      if (page > 1) setPage(page - 1);
-   };
-  
-
-    function nextPage() {
-        setPage(page + 1);
-    };
-
-    return(
-      <>
+  return (
+    <>
       {console.log("movies")}
-      <MovieNavBar setRender={setRender} setPage={setPage}/>      
-      <div style={{paddingTop:'1%'}}>
-        <h1 className='pageHeader'>Movies</h1>
+      <MovieNavBar setRender={setRender} setPage={setPage} />
+      <div style={{ paddingTop: "1%" }}>
+        <h1 className="pageHeader">Movies</h1>
         <Dropdown className="d-inline mx-2 dropdownStyle">
-          <Dropdown.Toggle id="dropdown-autoclose-true"  variant="dark" >
-             Filter
+          <Dropdown.Toggle id="dropdown-autoclose-true" variant="dark">
+            Filter
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item onClick={descendingOrder}>Latest</Dropdown.Item> 
-             <Dropdown.Item onClick={ascendingOrder}>Oldest</Dropdown.Item>
-            
+            <Dropdown.Item onClick={descendingOrder}>Latest</Dropdown.Item>
+            <Dropdown.Item onClick={ascendingOrder}>Oldest</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
-        </div>
-        <br></br>
-       
-      {
-        !Array.isArray(data)||data.length===0?
-        <p className='non-found'>
-          No Results Found For{' '}
-          "{queryString.parse(location.search).title}"
-        </p>  
-          :
-          <div>
-        <Container className="cntCenter" >
-        <Row>
-          {
-           data&&data.length? data.map((val,i)=>{
-              return(
-                <Movie key={i} movie={data} i={i} />
-              )
-            }):null
-          }
-        </Row> 
-      </Container>
-     
-      <Pagination className="center">
-        <Pagination.Prev onClick={previousPage}/>
-        <Pagination.Item disabled={true}>{page}</Pagination.Item>
-        <Pagination.Next onClick={nextPage} />
-      </Pagination>
-      
       </div>
-      }
-      
+      <br></br>
+
+      {!Array.isArray(data) || data.length === 0 ? (
+        <p className="non-found">
+          No Results Found For "{queryString.parse(location.search).title}"
+        </p>
+      ) : (
+        <div>
+          <Container className="cntCenter">
+            <Row>
+              {data && data.length
+                ? data.map((val, i) => {
+                    return <Movie key={i} movie={data} i={i} />;
+                  })
+                : null}
+            </Row>
+          </Container>
+
+          <Pagination className="center">
+            <Pagination.Prev onClick={previousPage} />
+            <Pagination.Item disabled={true}>{page}</Pagination.Item>
+            <Pagination.Next onClick={nextPage} />
+          </Pagination>
+        </div>
+      )}
     </>
-    )
+  );
 }
