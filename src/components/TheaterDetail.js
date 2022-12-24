4c20f9d11632f82de23d6e93448e35a610e20db8
seog-jun <shong69@myseneca.ca>
2022-12-23 19:28:01 -0500

    Changed to james

--- src/components/TheaterDetail.js
+++ src/components/TheaterDetail.js
@@ -1,111 +1,102 @@
 /*eslint-disable*/
 
 import React from "react";
-import { useParams } from 'react-router-dom';
-import Loading from './Loading';
-import { useEffect, useState} from 'react';
-import TheaterNavBar from './TheaterNavBar';
-import { MapContainer, TileLayer, Marker ,Popup} from 'react-leaflet'
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+import { useEffect, useState } from "react";
+import TheaterNavBar from "./TheaterNavBar";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Card } from "react-bootstrap";
-import { Container} from 'react-bootstrap';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { Container } from "react-bootstrap";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png') 
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
-export default function TheaterDetail(){
+export default function TheaterDetail() {
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [render, setRender] = useState(false);
+  let { id2 } = useParams();
 
-      const [data,setData] = useState(null);
-      const [page, setPage] = useState(1);
-      const [loading, setLoading] = useState(true);
-      const [render,setRender] = useState(false);
-      let {id2} = useParams();
-   
-    useEffect(() => {
-       
+  useEffect(() => {
+    fetch(`https://mflix-james.herokuapp.com/api/theaters/${id2}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not fetch the requested theater info");
+        }
+        return res.json();
+      })
+      .then((result) => {
+        if (result.hasOwnProperty("_id")) {
+          setData(result);
+        } else {
+          setData(null);
+        }
+      })
+      .catch((err) => {
+        console.error(`Something is wrong while fetching city info - ${err}`);
+      })
+      .finally(() => setLoading(false));
+  }, [id2, data]);
 
-        fetch(`https://mflix-jun.herokuapp.com/api/theaters/${id2}`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(
-                        'Could not fetch the requested theater info'
-                    );
-                }
-                return res.json();
-            })
-            .then((result) => {
-                if (result.hasOwnProperty('_id')) {
-                   setData(result);
-                } else {
-                   setData(null);
-                }
-            })
-            .catch((err) => {
-                console.error(
-                    `Something is wrong while fetching city info - ${err}`
-                );
-            })
-            .finally(() => setLoading(false))
-           
-    }, [id2,data]);
+  if (loading) return <Loading />;
 
-   
-  
-    if (loading) return (<Loading />);
-
-       
-   
-    return(
-        <>
-         <TheaterNavBar setRender={setRender} setPage={setPage}/>     
-         <div className="mapStyle">
-         <Container className="p-3">
-         <Card bg='light'>
-                <Card.Body
-                    style={{
-                      
-                        backgroundSize: '100% 100%',
-                        backgroundRepeat: 'no-repeat',
-                    }}>
-                    <Card.Title>{data.location.address.street1}</Card.Title>
-                    <Card.Text>
-                        {data.location.address.city}{', '}
-                        {data.location.address.state}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-         <MapContainer
-                className='mt-3'
-                style={{ height: '400px' }}
-                center={[
-                    data.location.geo.coordinates[1],
-                    data.location.geo.coordinates[0],
-                ]}
-                zoom={13}
-                scrollWheelZoom={false}>
-                <TileLayer 
-                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' 
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                <Marker
-                    position={[
-                        data.location.geo.coordinates[1],
-                        data.location.geo.coordinates[0],
-                    ]}>
-                    <Popup>
-                        {data.location.address.street1}, {data.location.address.city}, {data.location.address.state}
-                    </Popup>
-                </Marker>
-            </MapContainer>
-            </Container>
-            </div>
-        </>
-        
-    )
+  return (
+    <>
+      <TheaterNavBar setRender={setRender} setPage={setPage} />
+      <div className="mapStyle">
+        <Container className="p-3">
+          <Card bg="light">
+            <Card.Body
+              style={{
+                backgroundSize: "100% 100%",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <Card.Title>{data.location.address.street1}</Card.Title>
+              <Card.Text>
+                {data.location.address.city}
+                {", "}
+                {data.location.address.state}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <MapContainer
+            className="mt-3"
+            style={{ height: "400px" }}
+            center={[
+              data.location.geo.coordinates[1],
+              data.location.geo.coordinates[0],
+            ]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker
+              position={[
+                data.location.geo.coordinates[1],
+                data.location.geo.coordinates[0],
+              ]}
+            >
+              <Popup>
+                {data.location.address.street1}, {data.location.address.city},{" "}
+                {data.location.address.state}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </Container>
+      </div>
+    </>
+  );
 }
--- src/components/Theaters.js
+++ src/components/Theaters.js
@@ -2,119 +2,124 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "./Loading";
-import { Card,Table,Pagination} from "react-bootstrap";
+import { Card, Table, Pagination } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import queryString from 'query-string';
-import TheaterNavBar from './TheaterNavBar';
+import queryString from "query-string";
+import TheaterNavBar from "./TheaterNavBar";
 
-export default function Theaters(){
-
+export default function Theaters() {
   const perPage = 12;
   const [page, setPage] = useState(1);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [render,setRender] = useState(false);
+  const [render, setRender] = useState(false);
   const [error, setError] = useState(null);
   let navigate = useNavigate();
 
-  
-      let url;
-      let query = queryString.parse(location.search);
+  let url;
+  let query = queryString.parse(location.search);
 
-      if (query.city) {
-      url = `https://mflix-jun.herokuapp.com/api/theaters?page=${page}&perPage=${perPage}&city=${query.city}`;
-        }
-      else {
-        url = `https://mflix-jun.herokuapp.com/api/theaters?page=${page}&perPage=${perPage}`;
-      }
+  if (query.city) {
+    url = `https://mflix-james.herokuapp.com/api/theaters?page=${page}&perPage=${perPage}&city=${query.city}`;
+  } else {
+    url = `https://mflix-james.herokuapp.com/api/theaters?page=${page}&perPage=${perPage}`;
+  }
 
-      useEffect(() => {
-        axios(url)
-        .then((response) => {
+  useEffect(() => {
+    axios(url)
+      .then((response) => {
         setData(response.data);
-        })
-        .catch((error) => {
+      })
+      .catch((error) => {
         console.error("Error fetching data: ", error);
         setError(error);
-        })
-        .finally(() => {
+      })
+      .finally(() => {
         setLoading(false);
-        });
+      });
 
-      return setRender(false);
-      }, [page,error,render,data]);
-      
-      if (loading) return (<Loading />);
-      if (error) return  (
-        <Card className='p-3'style={{ width: '100rem' }}>
-            <Card.Title>Something is wrong while fetching data</Card.Title>
-            <Card.Text>{error.message}</Card.Text>
-        </Card>
+    return setRender(false);
+  }, [page, error, render, data]);
+
+  if (loading) return <Loading />;
+  if (error)
+    return (
+      <Card className="p-3" style={{ width: "100rem" }}>
+        <Card.Title>Something is wrong while fetching data</Card.Title>
+        <Card.Text>{error.message}</Card.Text>
+      </Card>
     );
 
-    function previousPage() {
-      if (page > 1) setPage(page - 1);
-   };
-  
-    function nextPage() {
-        setPage(page + 1);
-    };
-  
-    return (
-        <>
-         <TheaterNavBar setRender={setRender} setPage={setPage}/>      
-        <h1 style={{fontSize:25, color:'black', fontWeight:'bold',paddingLeft:'10%',paddingTop:'1%',marginBottom:0}}>Theaters</h1>
-        {
-        !Array.isArray(data)||data.length===0?
-        <p className='non-found'>
-          No Results Found For{' '}
-          "{queryString.parse(location.search).city}"
-        </p>  
-        
-          :
-         <div> 
+  function previousPage() {
+    if (page > 1) setPage(page - 1);
+  }
+
+  function nextPage() {
+    setPage(page + 1);
+  }
+
+  return (
+    <>
+      <TheaterNavBar setRender={setRender} setPage={setPage} />
+      <h1
+        style={{
+          fontSize: 25,
+          color: "black",
+          fontWeight: "bold",
+          paddingLeft: "10%",
+          paddingTop: "1%",
+          marginBottom: 0,
+        }}
+      >
+        Theaters
+      </h1>
+      {!Array.isArray(data) || data.length === 0 ? (
+        <p className="non-found">
+          No Results Found For "{queryString.parse(location.search).city}"
+        </p>
+      ) : (
+        <div>
           <div className="tableStyle">
-          <Table striped hover bordered className='mt-3'>
-            <thead>
-              <tr>
-                <th className='col-1'>Id</th>
-                <th className='col-3'>Address</th>
-                <th className='col-2'>City</th>
-                <th className='col-1'>State</th>
-                <th className='col-1'>Zip Code</th>
-              </tr>
-            </thead>
-            <tbody>
-
-            {data&&data.length? data.map((val,i)=>{
-              return(
-               
-                 <tr key={i} className="linkStyle" onClick={()=>{navigate('/theaters/'+val._id)}}>
-                   
-                    <td>{val.theaterId}</td>
-                    <td>{val.location.address.street1}</td>
-                    <td>{val.location.address.city}</td>
-                    <td>{val.location.address.state}</td>
-                    <td>{val.location.address.zipcode}</td>
-                    
-                 </tr>
-                    
-              )
-            }):null
-            }
-            </tbody>
-          </Table>
+            <Table striped hover bordered className="mt-3">
+              <thead>
+                <tr>
+                  <th className="col-1">Id</th>
+                  <th className="col-3">Address</th>
+                  <th className="col-2">City</th>
+                  <th className="col-1">State</th>
+                  <th className="col-1">Zip Code</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data && data.length
+                  ? data.map((val, i) => {
+                      return (
+                        <tr
+                          key={i}
+                          className="linkStyle"
+                          onClick={() => {
+                            navigate("/theaters/" + val._id);
+                          }}
+                        >
+                          <td>{val.theaterId}</td>
+                          <td>{val.location.address.street1}</td>
+                          <td>{val.location.address.city}</td>
+                          <td>{val.location.address.state}</td>
+                          <td>{val.location.address.zipcode}</td>
+                        </tr>
+                      );
+                    })
+                  : null}
+              </tbody>
+            </Table>
+          </div>
+          <Pagination className="center">
+            <Pagination.Prev onClick={previousPage} />
+            <Pagination.Item disabled={true}>{page}</Pagination.Item>
+            <Pagination.Next onClick={nextPage} />
+          </Pagination>
         </div>
-         <Pagination className="center">
-         <Pagination.Prev onClick={previousPage}/>
-         <Pagination.Item disabled={true}>{page}</Pagination.Item>
-         <Pagination.Next onClick={nextPage} />
-         </Pagination>
-         </div> 
-      }
-        
-         
-        </>
-
-        )
+      )}
+    </>
+  );
 }
